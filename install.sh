914352465209b66f534ee63c4c3c53514c187ba8
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-03-12 18:43:39 -0600

    WIP- Add install script to setup.py

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-pip install torch-scatter torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+#!/usr/bin/env bash
+
 pip install -r requirements.txt
-python setup.py install
+pip install git+https://github.com/snap-stanford/deepsnap.git
+pip install git+https://github.com/symbench/spice-completion.git
+pip install torch-scatter git+https://github.com/rusty1s/pytorch_sparse.git -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
