732fbfa22e2036f4d8fb6c475466d1fd60d65b32
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-03-15 09:04:16 -0500

    Modify install script. Closes #1 (#2)
    * Modify py38 install script. Closes #1
    
    * WIP- Add install script to setup.py
    
    * WIP- Add install script to setup.py
    
    * "python -m pip" instaead of "pip"
    
    Co-authored-by: Brian Broll <brian.broll@gmail.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
-pip install -e .
-pip install -r requirements.txt
+#!/usr/bin/env bash
+
+python -m pip install -r requirements.txt
+python -m pip install git+https://github.com/snap-stanford/deepsnap.git
+python -m pip install git+https://github.com/symbench/spice-completion.git
+python -m pip install torch-scatter git+https://github.com/rusty1s/pytorch_sparse.git -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
