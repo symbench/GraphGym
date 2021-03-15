cac88ed41664c6a2e712e70f1174a1761981eb18
Brian Broll <brian.broll@gmail.com>
2021-03-15 09:04:31 -0500

    "python -m pip" instaead of "pip"

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip install -r requirements.txt
-pip install git+https://github.com/snap-stanford/deepsnap.git
-pip install git+https://github.com/symbench/spice-completion.git
-pip install torch-scatter git+https://github.com/rusty1s/pytorch_sparse.git -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+python -m pip install -r requirements.txt
+python -m pip install git+https://github.com/snap-stanford/deepsnap.git
+python -m pip install git+https://github.com/symbench/spice-completion.git
+python -m pip install torch-scatter git+https://github.com/rusty1s/pytorch_sparse.git -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
