64e0f365b09b682469e217315f1e90c6703b7f23
Brian Broll <brian.broll@gmail.com>
2021-03-17 11:48:06 -0500

    Add spice-completion dependency to install

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ python -m pip install -r requirements.txt
 python -m pip install git+https://github.com/snap-stanford/deepsnap.git
 python -m pip install git+https://github.com/symbench/spice-completion.git
 python -m pip install torch-scatter git+https://github.com/rusty1s/pytorch_sparse.git -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+python -m pip install git+git://github.com/symbench/spice-completion.git
